fa0136eae51a93b2610e3528dc357af3abdd5ea8
Ryuta Otaki <otaki.ryuta@classmethod.jp>
2014-06-27 14:49:24 +0900

    fix container name

--- docker-ghost/recipes/deploy.rb
+++ docker-ghost/recipes/deploy.rb
@@ -1,6 +1,6 @@
 docker_image 'dockerfile/ghost:latest' do
   action :pull
-  notifies :redeploy,'docker_container[dockerfile/ghost]', :immediately
+  notifies :redeploy,'docker_container[ghost]', :immediately
 end
 docker_container 'ghost' do
   image 'dockerfile/ghost'
