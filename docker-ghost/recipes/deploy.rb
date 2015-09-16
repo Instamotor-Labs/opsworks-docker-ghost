0eda7a2805fa2886a08e2fd95bd0000decfc4a96
Thomas Goddard <thomas@instamotor.com>
2015-09-16 14:24:41 -0700

    Updating to new official ghost container on docker hub.

--- docker-ghost/recipes/deploy.rb
+++ docker-ghost/recipes/deploy.rb
@@ -1,9 +1,9 @@
-docker_image 'dockerfile/ghost:latest' do
+docker_image 'ghost:latest' do
   action :pull
   notifies :redeploy,'docker_container[ghost]', :immediately
 end
 docker_container 'ghost' do
-  image 'dockerfile/ghost'
+  image 'ghost'
   action :run
   port "80:2368"
   detach true
